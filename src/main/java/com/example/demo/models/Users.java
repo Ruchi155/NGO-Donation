71f92bfde84a5e45270c475e64e4f7882a5e1959
AnhMinh Le <minhbac333studyus@gmail.com>
2021-08-16 10:20:46 -0700

    Updated pom, lombok and donation, modified some user and role with lombok function

--- src/main/java/com/example/demo/models/Users.java
+++ src/main/java/com/example/demo/models/Users.java
@@ -16,16 +16,23 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-@Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
-public class Users {
-		
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+@Data
+@Entity(name = "Users")
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"email"}), name = "Users")
+@AllArgsConstructor
+@Builder
+public class Users { 
 		@Id
 	    @GeneratedValue(strategy = GenerationType.AUTO)
+		@Column(name = "user_id")
 	    private Long id;
 
 	    private String firstName;
 	    private String lastName;
+	    @Column(name = "email")
 	    private String email;
 	    private String password;
 
@@ -33,7 +40,7 @@ public class Users {
 	    @JoinTable(
 	            name = "users_roles",
 	            joinColumns = @JoinColumn(
-	                    name = "user_id", referencedColumnName = "id"),
+	                    name = "user_id", referencedColumnName = "user_id"),
 	            inverseJoinColumns = @JoinColumn(
 	                    name = "role_id", referencedColumnName = "id"))
 	    private Collection<Role> roles;
@@ -54,67 +61,7 @@ public class Users {
 	        this.email = email;
 	        this.password = password;
 	        this.roles = roles;
-	    }
-	  
-	    /*public boolean hasRole(String role) {
-	    	boolean hasRole = false;
-	    	for(Role r: this.roles) {
-	    		if(r.getName().equals(role)) {
-	    			hasRole=true;
-	    		}
-	    	}
-	    	return hasRole;
-	    }*/
-	    
-	    public Long getId() {
-	        return id;
-	    }
-
-	    public void setId(Long id) {
-	        this.id = id;
-	    }
-
-	    public String getFirstName() {
-	        return firstName;
-	    }
-
-	    public void setFirstName(String firstName) {
-	        this.firstName = firstName;
-	    }
-
-	    public String getLastName() {
-	        return lastName;
-	    }
-
-	    public void setLastName(String lastName) {
-	        this.lastName = lastName;
-	    }
-
-	    public String getEmail() {
-	        return email;
-	    }
-
-	    public void setEmail(String email) {
-	        this.email = email;
-	    }
-
-	    public String getPassword() {
-	        return password;
-	    }
-
-	    public void setPassword(String password) {
-	        this.password = password;
-	    }
-	    
-	    
-
-	    public Collection<Role> getRoles() {
-			return roles;
-		}
-
-		public void setRoles(Collection<Role> roles) {
-			this.roles = roles;
-		}
+	    }  
 
 		@Override
 	    public String toString() {
