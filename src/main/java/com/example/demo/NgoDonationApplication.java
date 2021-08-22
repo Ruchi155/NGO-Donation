277c9bd9b5c57655e245f744c541fd826a780961
Ruchi155 <patelruchi155@gmail.com>
2021-08-22 15:10:08 -0400

    update usercontroller

--- src/main/java/com/example/demo/NgoDonationApplication.java
+++ src/main/java/com/example/demo/NgoDonationApplication.java
@@ -34,6 +34,7 @@ public class NgoDonationApplication extends SpringBootServletInitializer  {
 		CorsConfiguration corsConfiguration= new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("content-type"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin", "Content-Type",
 					"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 					"Access-Control-Request-Method", "Access-Control-Request-Headers"));
